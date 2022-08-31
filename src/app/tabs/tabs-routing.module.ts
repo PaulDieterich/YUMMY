8145f030384fc7789eeb3845d29751a76e58a544
PaulDieterich <Paul.dieterich0@gmail.com>
2022-08-31 13:08:21 +0200

    change routing to new tabs

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,27 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'recipes',
+        loadChildren: () => import('../recipes/recipes.module').then(m => m.RecipesPageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'meals',
+        loadChildren: () => import('../meals/meals.module').then(m => m.MealsPageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'plans',
+        loadChildren: () => import('../plans/plans.module').then(m => m.PlansPageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/recipes',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/recipes',
     pathMatch: 'full'
   }
 ];
