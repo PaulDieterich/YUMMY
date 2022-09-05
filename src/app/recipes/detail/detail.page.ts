ea69a412eaf478d8c841284df6f0663c5c1e9389
PaulDieterich <Paul.dieterich0@gmail.com>
2022-09-05 18:14:29 +0200

    detail page, with dummy data

--- src/app/recipes/detail/detail.page.ts
+++ src/app/recipes/detail/detail.page.ts
@@ -1,4 +1,8 @@
+import { Ingredient } from './../ingredient.class';
+import { Recipe } from './../recipe.class';
 import {Component, OnInit} from '@angular/core';
+import { RecipesService } from '../recipes.service';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-detail',
@@ -6,11 +10,39 @@ import {Component, OnInit} from '@angular/core';
 	styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
-
-	constructor() {
+	recipe: Recipe = new Recipe();
+	dummyRecipe: Recipe = new Recipe();
+	nudeln: Ingredient = new Ingredient();
+	paprikla: Ingredient = new Ingredient();
+	constructor(private recipes: RecipesService) {
 	}
 
 	ngOnInit() {
+		this.dummyData();
+		console.log('ngOnInit');
+		this.recipes.get(1).then(recipe =>{
+			this.recipe = recipe;
+			console.log(`recipe ${recipe}`);
+		});
 	}
-
+	editRecipe(){
+		console.log('edit recipe');
+	}
+	dummyData(){
+		this.nudeln.setAmount(500);
+		this.nudeln.setName('Nudeln');
+		this.nudeln.setUnit('g');
+		this.paprikla.setAmount(3);
+		this.paprikla.setName('paprikla');
+		this.paprikla.setUnit('stück');
+		this.dummyRecipe.setId(99);
+		this.dummyRecipe.setName('nudelsalat');
+		this.dummyRecipe.setInstructions(['koch nudeln', 'alles klein schneiden']);
+		this.dummyRecipe.setIngredients([this.nudeln,this.paprikla]);
+		this.dummyRecipe.setPreparationTime('20min');
+		this.dummyRecipe.setCookingTime('20min');
+	}
 }
+
+
+
