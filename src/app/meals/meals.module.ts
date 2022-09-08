403afe5b98c342c698a07582a981d310e5800b03
PaulDieterich <Paul.dieterich0@gmail.com>
2022-09-08 10:44:12 +0200

    delete unsused includes

--- src/app/meals/meals.module.ts
+++ src/app/meals/meals.module.ts
@@ -3,7 +3,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {MealsPage} from './meals.page';
-import {ExploreContainerComponentModule} from '../explore-container/explore-container.module';
 import {MealsPageRoutingModule} from './meals-routing.module';
 
 @NgModule({
@@ -11,7 +10,6 @@ import {MealsPageRoutingModule} from './meals-routing.module';
 		IonicModule,
 		CommonModule,
 		FormsModule,
-		ExploreContainerComponentModule,
 		MealsPageRoutingModule
 	],
 	declarations: [MealsPage]
