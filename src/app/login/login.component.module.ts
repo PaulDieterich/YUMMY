e6a64074dad53bb23bdb655f40822b30100a188f
PaulDieterich <Paul.dieterich0@gmail.com>
2022-09-10 14:08:51 +0200

    edit login

--- src/app/login/login.component.module.ts
+++ src/app/login/login.component.module.ts
@@ -1,16 +1,19 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { LoginComponent } from './login.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    NgModule,
     IonicModule
   ],
-  declarations: []
+  declarations: [LoginComponent]
 })
-export class LoginPageModule {}
+export class LoginComponentModule {}
