84d042f037675a1793275d1e73e697b6cedf8dc0
Domenik Kranke <12494928+dokranke@users.noreply.github.com>
2022-09-11 21:10:07 +0200

    Struktur

---
+++ src/app/login/user.class.ts
@@ -0,0 +1,33 @@
+import { Recipe } from '../recipes/recipe.class';
+import {Entity} from './entity.class';
+
+export class User implements Entity {
+
+    public id: number;
+    public name: string;
+    public role: string;
+    public password: string;
+    public readonly recipes: Array<Recipe>;
+    constructor() {
+        this.id = -1;
+        this.name = '';
+        this.role = '';
+        this.password = '';
+    }
+    getId(): number {
+        return this.id;
+    }
+    public apply(user: User) {
+		this.id = user.id;
+        this.name = user.name;
+        this.role = user.role;
+        this.password = user.password;
+        this.recipes.splice(0, this.recipes.length, ...user.recipes);
+	}
+}
+export enum UserAttribute {
+	id = 'id',
+	name = 'name',
+	role = 'role',
+    recipe = 'recipe'
+}
---
+++ src/app/login/user.service.ts
@@ -0,0 +1,88 @@
+import { Recipe } from '../recipes/recipe.class';
+import { User, UserAttribute } from './user.class';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { API } from './api.class';
+import { Filter, Pagination, Sorter } from './list.class';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+
+  private http: HttpClient;
+  private service: ApiService<User, UserAttribute>;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+    this.service = new ApiService<User, UserAttribute>(http, '/users');
+  }
+  auth(user: string, password: string): UserService {
+		this.service.auth(user, password);
+		return this;
+	}
+
+  list(filters?: Filter<UserAttribute>[], sorter?: Sorter<UserAttribute>[], pagination?: Pagination): Observable<User[]> {
+		console.log('list');
+    return this.service.auth(this.service.user, this.service.password).list(filters, sorter, pagination);
+	}
+
+  get(id: number): Observable<User> {
+    console.log('get');
+   return new Observable<User>(observer => {
+      const user = new User();
+      let count = 0;
+      this.service.auth(this.service.user, this.service.password).get(id)
+      .subscribe(data => {
+        user.apply(data);
+        if(++count === 2) {
+          observer.next(user);
+          observer.complete();
+        }
+      });
+      new API<Recipe[]>(this.http).get('/users/{id}/recipes', id).subscribe(data => {
+        if(data) {
+          user.recipes.splice(0, user.recipes.length, ...data);
+        }
+        if(++count === 2) {
+          observer.next(user);
+          observer.complete();
+        }
+      });
+    });
+  }
+  create(user: User): Observable<User> {
+    return new Observable<User>(observer => {
+      let count = 0;
+      const  target = user.recipes.length + 1;
+      this.service.auth(this.service.user, this.service.password)
+      .create(user)
+      .subscribe(data => {
+        user.recipes.forEach(recipe => {
+          new API<Recipe>(this.http)
+          .auth(this.service.user, this.service.password)
+          .body(recipe)
+          .post('/users/{id}/recipes', data.id, recipe)
+          .subscribe(_ => {
+            if(++count === target){
+              observer.next(user);
+              observer.complete();
+            }
+          });
+        });
+        user.apply(data);
+        if(++count === target) {
+          observer.next(user);
+          observer.complete();
+        }
+     });
+    });
+  }
+  delete(id: number): Observable<boolean>{
+    return this.service.delete(id);
+  }
+}
